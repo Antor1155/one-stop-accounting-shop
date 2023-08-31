362d613e3082d108996e2084681d4494777d03b4
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-01 04:27:23 +0600

    initialized firebase with it@seg

--- assets/js/antormodified.js
+++ assets/js/antormodified.js
@@ -1,3 +1,9 @@
 window.onload = ()=>{
     document.getElementById("hide-on-load").style.opacity = "0"
 }
+
+document.getElementById("subscriptionForm").addEventListener("submit", e =>{
+    e.preventDefault()
+    console.log(e.target.email.value)
+    
+})
