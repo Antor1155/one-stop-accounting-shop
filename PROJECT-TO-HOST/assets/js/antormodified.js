d7cc66aa27acc14bd5f40ed236afdd538c519482
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-16 01:40:04 +0600

    updated upto making popup sectiona and form design

--- PROJECT-TO-HOST/assets/js/antormodified.js
+++ PROJECT-TO-HOST/assets/js/antormodified.js
@@ -7,3 +7,15 @@ document.getElementById("subscriptionForm").addEventListener("submit", e =>{
     console.log(e.target.email.value)
     
 })
+
+const popup = document.getElementById("submit-popup")
+popup.addEventListener("click", ()=>{
+    popup.classList.toggle("hidden")
+})
+
+const indexBigForm = document.getElementById("index-big-form")
+
+// indexBigForm.submit((e)=>{
+//     e.preventDefault()
+//     console.log(e.target)
+// })
