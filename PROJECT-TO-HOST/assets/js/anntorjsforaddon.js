8f451603958048b9429629923af72843d3e96421
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-22 21:45:58 +0600

    added anntorjsforaddon.js

--- PROJECT-TO-HOST/assets/js/anntorjsforaddon.js
+++ PROJECT-TO-HOST/assets/js/anntorjsforaddon.js
@@ -1,6 +1,12 @@
 // url for the backend
 const url = "https://us-central1-one-stop-accounting-published.cloudfunctions.net/app/formdata/"
 
+
+window.onload = ()=>{
+    document.getElementById("hide-on-load").style.opacity = "0"
+}
+
+
 // handle big-form on home page 
 document.getElementById("cform").addEventListener("submit", e=>{
     e.preventDefault()
@@ -26,4 +32,29 @@ document.getElementById("cform").addEventListener("submit", e=>{
         e.target.reset()
     })
 
+})
+
+
+// handle email submit for subscribe in home page 
+document.getElementById("subscriptionForm").addEventListener("submit", e =>{
+    e.preventDefault()
+    const data ={
+        email: e.target.email.value
+    } 
+    
+    fetch(`${url}subscription`, {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res =>{
+        
+        e.target.reset()
+    })
+    .catch(error => {
+        console.log(error)
+        e.target.reset()
+    })
 })
